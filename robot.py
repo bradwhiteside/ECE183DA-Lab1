5388009f24c91a0c870ca97e2cea4d802cda3847
Brad Whiteside <bradwhiteside@protonmail.com>
2021-01-22 10:22:48 -0800

    fix rotating feature again

--- robot.py
+++ robot.py
@@ -185,24 +185,36 @@ def loop(P, robot):
             angle = robot.S[2] * 180 / np.pi
             surf = pygame.Surface((w, d)).convert_alpha()
             surf.fill((0, 128, 255))
-            rotated_surf = pygame.transform.rotate(surf, angle)
-
-            # adjust coords so the surface rotates about its center
-            # https://stackoverflow.com/questions/4183208/how-do-i-rotate-an-image-around-its-center-using-pygame
-            box = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -d), (0, -d)]]
-            box_rotate = [p.rotate(angle) for p in box]
-            min_box = (min(box_rotate, key=lambda p: p[0])[0], min(box_rotate, key=lambda p: p[1])[1])
-            max_box = (max(box_rotate, key=lambda p: p[0])[0], max(box_rotate, key=lambda p: p[1])[1])
-            pivot = pygame.math.Vector2(w / 2, -d / 2)
-            pivot_rotate = pivot.rotate(angle)
-            pivot_move = pivot_rotate - pivot
             x = xOffset + robot.S[0]
             y = yOffset + robot.S[1]
-            origin = (x + min_box[0] - pivot_move[0],
-                      y - max_box[1] + pivot_move[1])
+            blitRotate(screen, surf, (x, y), (w//2, d//2), angle)
 
-            screen.blit(rotated_surf, origin)
             pygame.display.update()
+
+# adjust coords so the surface rotates about its center
+# https://stackoverflow.com/questions/4183208/how-do-i-rotate-an-image-around-its-center-using-pygame
+def blitRotate(surf, image, pos, originPos, angle):
+
+    # calcaulate the axis aligned bounding box of the rotated image
+    w, h       = image.get_size()
+    box        = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -h), (0, -h)]]
+    box_rotate = [p.rotate(angle) for p in box]
+    min_box    = (min(box_rotate, key=lambda p: p[0])[0], min(box_rotate, key=lambda p: p[1])[1])
+    max_box    = (max(box_rotate, key=lambda p: p[0])[0], max(box_rotate, key=lambda p: p[1])[1])
+
+    # calculate the translation of the pivot
+    pivot        = pygame.math.Vector2(originPos[0], -originPos[1])
+    pivot_rotate = pivot.rotate(angle)
+    pivot_move   = pivot_rotate - pivot
+
+    # calculate the upper left origin of the rotated image
+    origin = (pos[0] - originPos[0] + min_box[0] - pivot_move[0], pos[1] - originPos[1] - max_box[1] + pivot_move[1])
+
+    # get a rotated image
+    rotated_image = pygame.transform.rotate(image, angle)
+
+    # rotate and blit the image
+    surf.blit(rotated_image, origin)
 
 def main():
     # load parameters
